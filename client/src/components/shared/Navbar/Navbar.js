ebe72f79fd7c711b4d3afd319b7651d50b7cb37d
Ibtid <ibtid011@example.com>
2022-02-18 22:48:59 +0600

    changed navbar for logged in user

--- client/src/components/shared/Navbar/Navbar.js
+++ client/src/components/shared/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import searchIcon from '../../../assets/icons/magnifyingGlass.svg';
+import avatar from '../../../assets/icons/avatar.svg';
+import bellTcon from '../../../assets/icons/bellIcon.svg';
 
 import './Navbar.css';
 
@@ -24,10 +26,27 @@ const Navbar = (props) => {
           <img src={searchIcon} alt='search' />
         </div>
       </div>
-      <div
-        className={`navbar__authSection ${toggleDisplay ? '' : 'no__display'}`}>
-        <div className='navbar__signup'>Sign Up</div>
-      </div>
+      {!props.user && (
+        <div
+          className={`navbar__authSection ${
+            toggleDisplay ? '' : 'no__display'
+          }`}>
+          <div className='navbar__signup'>Sign Up</div>
+        </div>
+      )}
+      {props.user && (
+        <div
+          className={`navbar__authSection navbar__flexRow ${
+            toggleDisplay ? '' : 'no__display'
+          }`}>
+          <div className='navbar__iconButton' style={{ marginRight: '5vw' }}>
+            <img src={bellTcon} className='navbar__icon' alt='search' />
+          </div>
+          <div className='navbar__iconButton' style={{ marginRight: '5vw' }}>
+            <img src={avatar} className='navbar__icon' alt='search' />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
