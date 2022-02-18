dc60c440b29aa955950b0d00156908de5f5cf1d2
Ibtid <ibtid011@example.com>
2022-02-18 23:22:32 +0600

    style

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,13 @@ import SecondaryNav from './components/shared/SecondaryNav/SecondaryNav';
 import Sidebar from './components/shared/Sidebar/Sidebar';
 import Authentication from './pages/Authentication/Authentication';
 import Home from './pages/Home/Home';
+import ProductDetails from './pages/ProductDetails/ProductDetails';
 
 function App() {
   let signedIn = true;
-  return (
-    <div className='App'>
-      <Navbar user={signedIn} />
+
+  const componentWithSideBar = (component) => {
+    return (
       <div className='app_bodyWithSideBar'>
         <Sidebar />
         <div>
@@ -19,16 +20,35 @@ function App() {
               signedIn ? 'app__authenticated' : 'app__notAuthenticated'
             }>
             {/*Change Component*/}
-            <Home />
+            {component}
             <div className='home__scrollShade'></div>
           </div>
         </div>
       </div>
+    );
+  };
+
+  const componentWithSecondaryNav = (component) => {
+    return (
+      <div>
+        {signedIn && <SecondaryNav />}
+
+        <div
+          className={signedIn ? 'app__authenticated' : 'app__notAuthenticated'}>
+          {/*Change Component*/}
+          {component}
+          <div className='home__scrollShade'></div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className='App'>
+      <Navbar user={signedIn} />
+      {componentWithSideBar(<Home />)}
+      {/*componentWithSecondaryNav(<ProductDetails />)*/}
       {/*<Authentication />*/}
-      {/*My Profile */}
-      {/*Users Profile */}
-      {/*Home */}
-      {/*Productdetails*/}
     </div>
   );
 }
