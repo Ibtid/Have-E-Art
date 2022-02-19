38681ed480974cc388d9a4b5bb20746fee3bf2e1
Ibtid <ibtid011@example.com>
2022-02-19 10:46:19 +0600

    product details page done

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Sidebar from './components/shared/Sidebar/Sidebar';
 import Authentication from './pages/Authentication/Authentication';
 import Home from './pages/Home/Home';
 import ProductDetails from './pages/ProductDetails/ProductDetails';
+import backIcon from './assets/icons/backIcon.svg';
+import image1 from './assets/images/pexels-ayswarya-aish-2109147.jpg';
 
 function App() {
   let signedIn = true;
@@ -28,27 +30,32 @@ function App() {
     );
   };
 
-  const componentWithSecondaryNav = (component) => {
+  const componentWithOutSideBar = (component) => {
     return (
       <div>
-        {signedIn && <SecondaryNav />}
-
-        <div
-          className={signedIn ? 'app__authenticated' : 'app__notAuthenticated'}>
+        <div className='app__scroll'>
+          {signedIn && <SecondaryNav />}
           {/*Change Component*/}
-          {component}
-          <div className='home__scrollShade'></div>
+          <div className='app__goback'>
+            <img src={backIcon} />
+            <div>Go back</div>
+          </div>
+          <div className='app__bigImageComponent'>
+            <div className='app__bigImageContainer'>
+              <img className='app__bigImage' src={image1} alt='art' />
+            </div>
+            <div className='app__bigImageText'>{component}</div>
+          </div>
         </div>
       </div>
-
     );
   };
 
   return (
     <div className='App'>
       <Navbar user={signedIn} />
-      {componentWithSideBar(<Home />)}
-      {/*componentWithSecondaryNav(<ProductDetails />)*/}
+      {/*componentWithSideBar(<Home />)*/}
+      {componentWithOutSideBar(<ProductDetails />)}
       {/*<Authentication />*/}
     </div>
   );
