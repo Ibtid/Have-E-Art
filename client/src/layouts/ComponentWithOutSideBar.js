ef2a33c41b1b75db05f8be8371705949c1b82521
Ibtid <ibtid011@example.com>
2022-02-23 19:39:08 +0600

    context applied in layouts

--- client/src/layouts/ComponentWithOutSideBar.js
+++ client/src/layouts/ComponentWithOutSideBar.js
@@ -3,17 +3,21 @@ import image1 from '../assets/images/pexels-ayswarya-aish-2109147.jpg';
 import backIcon from '../assets/icons/backIcon.svg';
 import SecondaryNav from '../components/shared/SecondaryNav/SecondaryNav';
 import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AppContext } from '../hooks/AppContext';
 
 const ComponentWithOutSideBar = (props) => {
   let history = useNavigate();
+  const value = useContext(AppContext);
+
   return (
     <div className='app__scroll'>
-      {props.signedIn && <SecondaryNav />}
+      {value.loggedIn && <SecondaryNav />}
       {/*Change Component*/}
       <div
         className='app__goback'
         onClick={() => {
-          history(`${document.referrer.replace('http://localhost:3000', '')}`);
+          history(-1);
         }}>
         <img src={backIcon} />
         <div>Go back</div>
--- client/src/layouts/ComponentWithSideBar.js
+++ client/src/layouts/ComponentWithSideBar.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import SecondaryNav from '../components/shared/SecondaryNav/SecondaryNav';
 import Sidebar from '../components/shared/Sidebar/Sidebar';
+import { useContext } from 'react';
+import { AppContext } from '../hooks/AppContext';
 
 const ComponentWithSideBar = (props) => {
+  const value = useContext(AppContext);
   return (
     <div className='app_bodyWithSideBar'>
       <Sidebar />
       <div>
-        {props.signedIn && <SecondaryNav />}
+        {value.loggedIn && <SecondaryNav />}
         <div
           className={
-            props.signedIn ? 'app__authenticated' : 'app__notAuthenticated'
+            value.loggedIn ? 'app__authenticated' : 'app__notAuthenticated'
           }>
           {/*Change Component*/}
           {props.children}
