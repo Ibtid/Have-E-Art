53a6b59fc53aadfeaa743f06c083acac2a0a51b4
hafizsheetab <muhtaseen@iut-dhaka.edu>
2022-02-23 21:47:33 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const path = require('path');
 app.use(express.static("client/build"));
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
