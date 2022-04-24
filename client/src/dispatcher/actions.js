834ffd0ce42e2da8f9227e17e0e6018fc3a40047
hafizsheetab <muhtaseen@iut-dhaka.edu>
2022-04-24 14:31:34 +0600

    creating e art done

--- client/src/dispatcher/actions.js
+++ client/src/dispatcher/actions.js
@@ -2,7 +2,8 @@ const actions = {
     login: "LOGIN",
     signUp: "SIGN_UP", 
     getMyProfile: "GET_MY_PROFILE",
-    editProfile: "EDIT_PROFILE"
+    editProfile: "EDIT_PROFILE",
+    addEArt: "ADDEART"
 }
 
 export default actions
--- client/src/dispatcher/dispatch.js
+++ client/src/dispatcher/dispatch.js
@@ -1,7 +1,7 @@
 import actions from './actions';
 import axios from 'axios';
-const baseUrl = 'https://secret-dawn-63355.herokuapp.com';
-// const baseUrl = "http://localhost:5000"
+// const baseUrl = 'https://secret-dawn-63355.herokuapp.com';
+const baseUrl = "http://localhost:5000"
 
 const dispatch = async (action, headerParams = {}, body = {}, token = '') => {
  try{
@@ -38,6 +38,13 @@ const dispatch = async (action, headerParams = {}, body = {}, token = '') => {
       response = await axios(axiosOptions);
       return response.data;
       break
+    case actions.addEArt:
+      console.log(body)
+      axiosOptions = getAxiosOptions("POST", `${baseUrl}/eart`, body, token)
+      console.log(axiosOptions.data)
+      response = await axios(axiosOptions)
+      return response.data
+      break
   }
  }catch(err){
     return err.response.data
@@ -63,9 +70,7 @@ const getAxiosOptions = (method, url, body, token) => {
         method: 'POST',
         url,
         headers,
-        data: {
-          ...body,
-        },
+        data: body
       };
       break;
     case 'PUT':
