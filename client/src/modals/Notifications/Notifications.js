23f3902b01d50899366ae957c5ec54532dfe6c63
Ibtid <ibtid011@example.com>
2022-07-07 23:29:38 +0600

    push korlam

--- client/src/modals/Notifications/Notifications.js
+++ client/src/modals/Notifications/Notifications.js
@@ -1,85 +1,84 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
-import ReactDOM from "react-dom";
-import { useNavigate } from "react-router-dom";
-import image from "../../assets/images/pexels-dan-cristian-pădureț-1193743 1.png";
-import { AppContext } from "../../hooks/AppContext";
-import "./Notifications.css";
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { useNavigate } from 'react-router-dom';
+import image from '../../assets/images/pexels-dan-cristian-pădureț-1193743 1.png';
+import { AppContext } from '../../hooks/AppContext';
+import './Notifications.css';
 
 const Notifications = (props) => {
-    const { contextStore } = useContext(AppContext);
-    const ref = useRef();
-    const [showForm, setShowForm] = useState(true);
-    const navigate = useNavigate()
+  const { contextStore } = useContext(AppContext);
+  const ref = useRef();
+  const [showForm, setShowForm] = useState(true);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const checkIfClickedOutside = (e) => {
-            // If the menu is open and the clicked target is not within the menu,
-            // then close the menu
-            if (showForm && ref.current && !ref.current.contains(e.target)) {
-                setShowForm(false);
-                props.closeNotifications();
-            }
-        };
-
-        document.addEventListener("mousedown", checkIfClickedOutside);
-
-        return () => {
-            // Cleanup the event listener
-            document.removeEventListener("mousedown", checkIfClickedOutside);
-        };
-    }, [showForm]);
-    const onClickNotification = (notification) => {
-      console.log(notification)
-      setShowForm(false);
-      props.closeNotifications();
-      switch(notification.type){
-        case "user":
-          navigate(`/user/earts/GalleryShowcase/${notification.user}`)
-          break
-        case "eart":
-          break
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      // If the menu is open and the clicked target is not within the menu,
+      // then close the menu
+      if (showForm && ref.current && !ref.current.contains(e.target)) {
+        setShowForm(false);
+        props.closeNotifications();
       }
-     
+    };
+
+    document.addEventListener('mousedown', checkIfClickedOutside);
+
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener('mousedown', checkIfClickedOutside);
+    };
+  }, [showForm]);
+  const onClickNotification = (notification) => {
+    console.log(notification);
+    setShowForm(false);
+    props.closeNotifications();
+    switch (notification.type) {
+      case 'user':
+        navigate(`/user/earts/GalleryShowcase/${notification.user}`);
+        break;
+      case 'eart':
+        break;
     }
-    return ReactDOM.createPortal(
-        <div
-            className="notifications"
-            onClick={() => {
-                setShowForm(true);
-            }}
-        >
-            <div ref={ref}>
-                {showForm && (
-                    <div className="notifications__Container">
-                        <div className="notifications__title">
-                            Notifications
-                        </div>
-                        <div className="notifications__scroll">
-                            {contextStore.notifications.map((notification) => (
-                                <div className="notifications__oneNotification" onClick={() => {onClickNotification(notification)}}>
-                                    <img
-                                        className="notifications__image"
-                                        src={image}
-                                        alt=""
-                                    />
-                                    <div className="notifications__textContainer">
-                                        <div className="notifications__event">
-                                            <b>{notification.highlightOne}</b> {notification.noHighlight}{" "}
-                                            <b>{notification.highlightTwo}</b>
-                                        </div>
-                                        <div className="notifications__time">
-                                            5 hours ago
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
+  };
+  return ReactDOM.createPortal(
+    <div
+      className='notifications'
+      onClick={() => {
+        setShowForm(true);
+      }}>
+      <div ref={ref}>
+        {showForm && (
+          <div className='notifications__Container'>
+            <div className='notifications__title'>Notifications</div>
+            <div className='notifications__scroll'>
+              {contextStore.notifications.map((notification) => (
+                <div
+                  className='notifications__oneNotification'
+                  onClick={() => {
+                    onClickNotification(notification);
+                  }}>
+                  <img className='notifications__image' src={image} alt='' />
+                  <div className='notifications__textContainer'>
+                    <div className='notifications__event'>
+                      <b style={{ cursor: 'pointer' }}>
+                        {notification.highlightOne}
+                      </b>{' '}
+                      {notification.noHighlight}{' '}
+                      <b style={{ cursor: 'pointer' }}>
+                        {notification.highlightTwo}
+                      </b>
                     </div>
-                )}
+                    <div className='notifications__time'>5 hours ago</div>
+                  </div>
+                </div>
+              ))}
             </div>
-        </div>,
-        document.getElementById("notifications")
-    );
+          </div>
+        )}
+      </div>
+    </div>,
+    document.getElementById('notifications')
+  );
 };
 
 export default Notifications;
