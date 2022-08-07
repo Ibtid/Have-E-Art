4f84830e22a2b054b4de63e317f72118e923abcb
Ibtid <ibtid011@example.com>
2022-08-07 08:09:32 +0600

    redirect in user profile

--- client/src/components/cards/User/UserCard.js
+++ client/src/components/cards/User/UserCard.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import image1 from '../../../assets/icons/avatar.svg';
 import './UserCard.css';
-const UserCard = ({user}) => {
+import { useNavigate } from 'react-router-dom';
+const UserCard = ({ user }) => {
+  const navigate = useNavigate();
+  console.log(user);
   return (
-    <div className='userCard__container'>
+    <div
+      className='userCard__container'
+      onClick={() => {
+        navigate(`/user/earts/GalleryShowcase/${user._id}`);
+      }}>
       <div className='userCard__imageContainer'>
-        <img className='userCard__image' src={user.profileImage ? user.profileImage : image1} alt='user' />
+        <img
+          className='userCard__image'
+          src={user.profileImage ? user.profileImage : image1}
+          alt='user'
+        />
       </div>
       <div className='userCard__textContainer'>
-        <div className='userCard__Name'>{user.firstName +" " + user.lastName}</div>
-        <div className='userCard__OwnedArts'>Followers: {user.followers.length}</div>
+        <div className='userCard__Name'>
+          {user.firstName + ' ' + user.lastName}
+        </div>
+        <div className='userCard__OwnedArts'>
+          Followers: {user.followers.length}
+        </div>
       </div>
     </div>
   );
