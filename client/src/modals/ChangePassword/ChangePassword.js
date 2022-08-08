3c74bab7ed940803c3ddee1cbef18a414929fcb8
Ibtid <ibtid011@example.com>
2022-08-08 23:42:17 +0600

    ne validation

--- client/src/modals/ChangePassword/ChangePassword.js
+++ client/src/modals/ChangePassword/ChangePassword.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
+import './ChangePassword.css';
 
 import { SpinnerContext } from '../../hooks/SpinnerContext';
 
@@ -12,15 +13,42 @@ const ChangePassword = (props) => {
     oldPassword: '',
     confirmPassword: '',
   });
+  const [errors, setErrors] = useState({
+    newPassword: '',
+    oldPassword: '',
+    confirmPassword: '',
+  });
   const { setShowSpinner } = useContext(SpinnerContext);
 
   const onChangeFormData = (e) => {
     setformData({ ...formData, [e.target.name]: e.target.value });
   };
   const onClickSubmit = async () => {
-    if (formData.newPassword) {
-      setShowSpinner(true);
-      setShowSpinner(false);
+    let allErrors = { newPassword: '', oldPassword: '', confirmPassword: '' };
+    setShowSpinner(true);
+    if (formData.newPassword.length < 6) {
+      allErrors = { ...allErrors, newPassword: 'Use atleast 6 characters' };
+    }
+    if (formData.oldPassword.length < 6) {
+      allErrors = { ...allErrors, oldPassword: 'Use atleast 6 characters' };
+    }
+    if (formData.confirmPassword.length < 6) {
+      allErrors = { ...allErrors, confirmPassword: 'Use atleast 6 characters' };
+    }
+    if (formData.newPassword !== formData.confirmPassword) {
+      allErrors = {
+        ...allErrors,
+        newPassword: 'Password does not match',
+        confirmPassword: 'Password does not match',
+      };
+    }
+    setShowSpinner(false);
+    setErrors(allErrors);
+    if (
+      errors.confirmPassword.length === 1 &&
+      errors.newPassword.length === 1 &&
+      errors.oldPassword.length === 1
+    ) {
       setPop('pop__down');
       setTimeout(() => {
         props.closeForm();
@@ -63,28 +91,45 @@ const ChangePassword = (props) => {
             }}>
             <div className='cardDetails__label'>Old Password</div>
             <input
-              className='cardDetails__inputBig'
+              className={`cardDetails__inputBig ${
+                errors.oldPassword.length > 1 ? 'red__border' : ''
+              }`}
               name='oldPassword'
               type='password'
               value={formData.oldPassword}
               onChange={onChangeFormData}
             />
+            {errors.oldPassword.length > 1 && (
+              <div className='changePassword__error'>{errors.oldPassword}</div>
+            )}
             <div className='cardDetails__label'>New Password</div>
             <input
-              className='cardDetails__inputBig'
+              className={`cardDetails__inputBig ${
+                errors.newPassword.length > 1 ? 'red__border' : ''
+              }`}
               name='newPassword'
               type='password'
               value={formData.newPassword}
               onChange={onChangeFormData}
             />
+            {errors.newPassword.length > 1 && (
+              <div className='changePassword__error'>{errors.newPassword}</div>
+            )}
             <div className='cardDetails__label'>Confirm Password</div>
             <input
-              className='cardDetails__inputBig'
+              className={`cardDetails__inputBig ${
+                errors.confirmPassword.length > 1 ? 'red__border' : ''
+              }`}
               name='confirmPassword'
               type='password'
               value={formData.confirmPassword}
               onChange={onChangeFormData}
             />
+            {errors.confirmPassword.length > 1 && (
+              <div className='changePassword__error'>
+                {errors.confirmPassword}
+              </div>
+            )}
             <div className='newGroup__button'></div>
             <div className='cardDetails__button' onClick={onClickSubmit}>
               Change Password
