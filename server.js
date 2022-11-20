c9c3cd9f9e3b0c84ca2a7feb3785fa70761b53a0
hafizsheetab <56652043+hafizsheetab@users.noreply.github.com>
2022-11-20 21:21:13 +0100

    port number changed

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
-const PORT = process.env.PORT || 6002;
+const PORT = process.env.PORT || 6003;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
